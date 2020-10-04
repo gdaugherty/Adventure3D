2c8bc2ae767a176f1e33ce6368d8a2b3960ee942
gdaugherty <garyd@undertonefx.com>
2020-10-04 16:04:25 -0700

    Spawning and Bridge

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,23 @@ public class GameManager : MonoBehaviour
 {
     // Place holders to allow connecting to other objects
     //A reference to the game manager
-    
+
     public Transform spawnPoint;
     public GameObject player;
     public GameObject DeathScreen;
     public GameObject WinScreen;
+
+    public GameObject[] GoldKeySpawnpoints = null;
+    public GameObject[] WhiteKeySpawnpoints = null;
+    public GameObject[] BlackKeySpawnpoints = null;
+    public GameObject[] ChaliceSpawnpoints = null;
+    public GameObject[] SwordSpawnpoints = null;
+
+    public GameObject GoldKey;
+    public GameObject WhiteKey;
+    public GameObject BlackKey;
+    public GameObject Chalice;
+    public GameObject Sword;
 
     // Flags that control the state of the game
     private bool isRunning = false;
@@ -31,6 +43,16 @@ public class GameManager : MonoBehaviour
         // Disables controls at the start.
         fpsController.enabled = false;
         PositionPlayer();
+        SpawnObjects();
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     //Runs when the player needs to be positioned back at the spawn point
@@ -53,6 +75,12 @@ public class GameManager : MonoBehaviour
         CarriedObject.transform.parent = null;
         CarriedObject = GameObject.Find("BlackKey");
         CarriedObject.transform.parent = null;
+        CarriedObject = GameObject.Find("WhiteKey");
+        CarriedObject.transform.parent = null;
+        CarriedObject = GameObject.Find("Bridge");
+        CarriedObject.transform.parent = null;
+        CarriedObject = GameObject.Find("Magnet");
+        CarriedObject.transform.parent = null;
         CarriedObject = GameObject.Find("Chalice");
         CarriedObject.transform.parent = null;
     }
@@ -65,6 +93,16 @@ public class GameManager : MonoBehaviour
         isDead = false;
 
         fpsController.enabled = true;
+        
+    }
+
+    private void SpawnObjects()
+    {
+        GoldKey.transform.position = GoldKeySpawnpoints[Random.Range(0, 4)].transform.position;
+        WhiteKey.transform.position = WhiteKeySpawnpoints[Random.Range(0, 4)].transform.position;
+        BlackKey.transform.position = BlackKeySpawnpoints[Random.Range(0, 4)].transform.position;
+        Chalice.transform.position = ChaliceSpawnpoints[Random.Range(0, 4)].transform.position;
+        Sword.transform.position = SwordSpawnpoints[Random.Range(0, 4)].transform.position;
     }
 
     private void ResumeGame()
