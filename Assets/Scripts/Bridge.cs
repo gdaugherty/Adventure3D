99b6aeb2eb0a9d07ad3604b87577d1a3cbb002c6
gdaugherty <garyd@undertonefx.com>
2020-10-11 17:05:56 -0700

    Added Bridge scripting and Dragon Attack model

---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    public Transform PortalSpawn;  
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject CollidedWith = collider.gameObject;
+
+        if (CollidedWith.tag == "Player")
+        {
+            CollidedWith.transform.position = PortalSpawn.position;
+        }
+                           
+    }
+   
+}
