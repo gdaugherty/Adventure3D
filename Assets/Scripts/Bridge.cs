600aad98218c32eacd2d43dd4c9d033813484201
gdaugherty <garyd@undertonefx.com>
2020-10-13 18:49:58 -0700

    Added Navmesh AI

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Bridge : MonoBehaviour
 {
-    public Transform PortalSpawn;  
-
+    public Transform PortalSpawn;
+   
     void OnTriggerEnter(Collider collider)
     {
-        GameObject CollidedWith = collider.gameObject;
-
-        if (CollidedWith.tag == "Player")
+        if (collider.tag == "Player")
         {
-            CollidedWith.transform.position = PortalSpawn.position;
+            collider.transform.position = PortalSpawn.position;            
         }
                            
     }
